f4f94062913def7b4a70128bd4386f49c2c39ff5
thedtj <awesomedan@gmail.com>
2019-05-09 13:33:00 -0700

    created image data table

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,24 +5,28 @@
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
 
-import Header from "./header"
-import "./layout.css"
+import Header from './header'
+import './layout.css'
+
+const GET_SITEMETADATA = graphql`
+  query SiteTitleQuery {
+    site {
+      siteMetadata {
+        title
+        author
+        createdAt
+      }
+    }
+  }
+`
 
 const Layout = ({ children }) => (
   <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
+    query={GET_SITEMETADATA}
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
@@ -36,9 +40,9 @@ const Layout = ({ children }) => (
         >
           <main>{children}</main>
           <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
+            {`Built by ${data.site.siteMetadata.author} © ${
+              data.site.siteMetadata.createdAt
+            }`}
           </footer>
         </div>
       </>
---
+++ src/pages/posts/page-3.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { graphql, StaticQuery, Link } from 'gatsby'
+import Layout from '../../components/layout'
+
+const GET_IMAGE_DATA = graphql`
+  {
+    allFile {
+      totalCount
+      edges {
+        node {
+          relativePath
+          size
+          extension
+          birthTime
+        }
+      }
+    }
+  }
+`
+
+export default () => (
+  <Layout>
+    <h1>Hello from the third page!</h1>
+    <h3>Image File Data</h3>
+    <StaticQuery
+      query={GET_IMAGE_DATA}
+      render={data => (
+        <table>
+          <thead>
+            <tr>
+              <th>Relative Path</th>
+              <th>Size of Image</th>
+              <th>Extension</th>
+              <th>BirthTime</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.allFile.edges.map(({ node }, index) => (
+              <tr key={index}>
+                <td>{node.relativePath}</td>
+                <td>{node.size}</td>
+                <td>{node.extension}</td>
+                <td>{node.birthTime}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    />
+    <Link to="/page-2">Go to page 2.</Link>
+  </Layout>
+)
